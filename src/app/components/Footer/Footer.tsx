80f9a6b6be3e7c59c673ab3cbf9308408e4f3419
matheus-kiyoshi <matheus.kiyoshi.f@gmail.com>
2023-08-13 13:26:54 -0300

    initial commit

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import '@/app/globals.css'
+
+export default function Footer() {
+  return (
+    <div className="grid-area-footer bg-blue-500">
+      <p>Footer</p>
+    </div>
+  )
+}
---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import '@/app/globals.css'
+
+export default function Header() {
+  return (
+    <div className="grid-area-header bg-blue-500">
+      <p>Header</p>
+    </div>
+  )
+}
