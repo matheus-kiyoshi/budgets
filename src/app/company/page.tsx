f42501a6cbd3e3ee7d5b3f4032732167011ce261
matheus-kiyoshi <matheus.kiyoshi.f@gmail.com>
2023-08-13 14:37:31 -0300

    company page and template added for future changes

---
+++ src/app/company/page.tsx
@@ -0,0 +1,25 @@
+import '@/app/globals.css'
+
+export default function Company() {
+  return (
+    <main className="grid-area-main m-14 flex flex-col justify-start items-start gap-16">
+      <h1 className="text-7xl">Company</h1>
+      <p className="text-justify text-xl p-8">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illum
+        numquam est saepe iure officiis, iste eligendi cumque. Voluptate laborum
+        blanditiis voluptatem iure officiis exercitationem mollitia tenetur
+        quidem corporis quia. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Modi illum numquam est saepe iure officiis, iste eligendi cumque.
+        Voluptate laborum blanditiis voluptatem iure officiis exercitationem
+        mollitia tenetur quidem corporis quia. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Modi illum numquam est saepe iure
+        officiis, iste eligendi cumque. Voluptate laborum blanditiis voluptatem
+        iure officiis exercitationem mollitia tenetur quidem corporis quia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illum
+        numquam est saepe iure officiis, iste eligendi cumque. Voluptate laborum
+        blanditiis voluptatem iure officiis exercitationem mollitia tenetur
+        quidem corporis quia.
+      </p>
+    </main>
+  )
+}
